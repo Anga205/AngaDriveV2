db10419b9ad12f53f3b969d00a5f04666b8b7b6a
Anga205 <angadbhalla2005@gmail.com>
2024-07-11 10:20:50 +0530

    added startup script for debian

---
+++ debian.sh
@@ -0,0 +1,19 @@
+# List of packages to search for
+PACKAGES=("git" "python3-venv" "curl" "nodejs" "gcc" "python3-dev" "unzip")
+
+# Loop through the packages and check if they are installed
+for package in "${PACKAGES[@]}"; do
+    if ! dpkg -s "$package" >/dev/null 2>&1; then
+        echo "Package $package is not installed. Installing..."
+        sudo apt install -y "$package"
+    fi
+done
+
+
+if [ ! -d "venv" ]; then
+    python3 -m venv venv 
+    source venv/bin/activate
+    pip install -r requirements.txt
+    reflex init
+fi
+reflex run
