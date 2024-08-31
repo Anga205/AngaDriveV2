61c5cc6e33873a76175cf64b0fb52ae4ac9a3047
Anga205 <angadbhalla2005@gmail.com>
2024-08-31 22:41:20 +0530

    added upload state

--- AngaDriveV2/mydrive.py
+++ AngaDriveV2/mydrive.py
@@ -2,10 +2,40 @@ import reflex as rx
 from AngaDriveV2.State import State
 from AngaDriveV2.shared_components import *
 
+class UploadState(State):
+    open_upload_dialog:bool = False
+    
+    def open_dialog(self):
+        self.open_upload_dialog = True
+
+    def close_dialog(self):
+        self.open_upload_dialog = False
+        return rx.clear_selected_files("file_page_upload")
+
+    async def handle_file_page_upload(self, files: list[rx.UploadFile]):
+        yield rx.clear_selected_files("file_page_upload")
+        file_link_list = []
+        for file in files:
+            upload_data = await file.read()
+            filename = gen_filename(file.filename)
+            outfile = os.path.join(file_directory,filename)
+            with open(outfile, "wb") as f:
+                f.write(upload_data)
+            file_link_list.append(file_link+os.path.join(filename.split(".")[0],file.filename))
+            add_file_to_database(
+                account_token=self.token,
+                file_directory=filename,
+                file_size=get_file_size(outfile),
+                original_file_name=file.filename
+            )
+        self.user_files: list[dict[str, str]] = get_all_user_files_for_display(self.token)
+        yield self.close_dialog()
+        yield rx.set_clipboard(", \n".join(file_link_list))
+        yield rx.toast.success("Files uploaded and copied to clipboard")
 
 def upload_button():
-    return rx.alert_dialog.root(
-    rx.alert_dialog.trigger(
+    return rx.dialog.root(
+    rx.dialog.trigger(
         rx.chakra.button(
             rx.chakra.image(
                 src="/upload.png",
@@ -21,14 +51,14 @@ def upload_button():
             width="10vh",
             border_radius="1vh",
             _hover={"bg":"#0000aa"},
-            on_click=rx.clear_selected_files("file_page_upload")
+            on_click=UploadState.open_dialog
         ),
     ),
-    rx.alert_dialog.content(
-        rx.alert_dialog.title(
+    rx.dialog.content(
+        rx.dialog.title(
             "Upload Files"
         ),
-        rx.alert_dialog.description(
+        rx.dialog.description(
             rx.upload(
                 rx.cond(
                     rx.selected_files("file_page_upload"),
@@ -53,45 +83,67 @@ def upload_button():
                 justify_content= 'center',
                 align_items= 'center',
                 padding="1vh",
+                border_radius="5px",
                 border="1px dotted #0000ff",
                 id="file_page_upload"
             )
         ),
-        rx.chakra.box(
+        rx.box(
             height="1vh"
         ),
+        rx.cond(
+            rx.selected_files("file_page_upload"),
+            empty_component(),
+            rx.vstack(
+                rx.hstack(
+                    rx.divider(),
+                    rx.text("OR"),
+                    rx.divider(),
+                    justify="center",
+                    align="center",
+                    width="100%"
+                ),
+                rx.chakra.input(
+                    placeholder="Enter file link",
+                    width="100%"
+                ),
+                rx.box(
+                    height="1vh"
+                ),
+                spacing="0",
+                width="100%",
+            )
+        ),
         rx.chakra.hstack(
-            rx.alert_dialog.cancel(
-                rx.chakra.button(
-                    "Close",
-                    bg="#440000",
-                    color="WHITE",
-                    _hover={"bg":"#000033"},
-                    on_click=rx.clear_selected_files("file_page_upload")
-                )
+            rx.chakra.button(
+                "Close",
+                bg="#440000",
+                color="WHITE",
+                _hover={"bg":"#000033"},
+                on_click=UploadState.close_dialog
             ),
             rx.spacer(),
-            rx.cond(
+            conditional_render(
                 rx.selected_files("file_page_upload"),
-                rx.alert_dialog.cancel(
+                rx.chakra.box(
                     rx.chakra.button(
                         "Upload",
                         bg="#113322",
                         color="WHITE",
                         _hover={"bg":"#224433"},
-                        on_click=State.handle_file_page_upload(rx.upload_files(upload_id="file_page_upload", on_upload_progress=State.upload_progressbar))
+                        on_click=UploadState.handle_file_page_upload(rx.upload_files(upload_id="file_page_upload", on_upload_progress=State.upload_progressbar))
                     )
                 ),
-                rx.box(
-                    width="0px", 
-                    height="0px"
-                ),
             ),
-            width="100%"
+            width="100%",
+            spacing="10"
         ),
         bg="#111111",
-        color="WHITE"
+        color="WHITE",
+        on_escape_key_down=lambda x: UploadState.close_dialog(),
+        on_pointer_down_outside=lambda x: UploadState.close_dialog()
     ),
+    open=UploadState.open_upload_dialog
 )
 
 def desktop_index():
@@ -175,7 +227,7 @@ def no_files():
     width="100%",
     height="95vh",
     padding="0px",
-    on_drop=State.handle_file_page_upload(rx.upload_files(upload_id="tablet_page_upload", on_upload_progress=State.upload_progressbar))
+    on_drop=UploadState.handle_file_page_upload(rx.upload_files(upload_id="file_page_upload", on_upload_progress=State.upload_progressbar))
 )
 
 def tablet_show_files():
@@ -189,7 +241,7 @@ def tablet_show_files():
             id="tablet_page_upload",
             border="0px",
             padding="0px",
-            on_drop=State.handle_file_page_upload(rx.upload_files(upload_id="tablet_page_upload", on_upload_progress=State.upload_progressbar))
+            on_drop=UploadState.handle_file_page_upload(rx.upload_files(upload_id="file_page_upload", on_upload_progress=State.upload_progressbar))
         ),
         width="90%"
     ),
